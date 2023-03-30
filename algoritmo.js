95c8ace463c97990e553796a17d793c7d685f1a4
Gustavo <gustavo_r69@yahoo.com.ar>
2023-03-30 17:45:00 -0300

    actualize readme

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+console.log(" Hello World¡ ");
+
+document.write(" algo ");
+
+document.write("<h1> Mi primer pagina </h1>"),
+
+document.write("<img src='https://www.gifss.com/aves/patos/images/Pato_06.gif ' alt='gif -img agregada mediante js?>"
